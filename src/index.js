ab2cc9a02f694b06e0de3981ddbc6067d6548d22
Eliran Pe'er <eliran.peer@forter.com>
2018-06-13 00:35:52 +0300

    redirect mobile devices to google play/appstore

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app.component';
 import './index.css';
+import isMobile from 'is-mobile';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+if (isMobile()) {
+    const isIOs = /iPad|iPhone|iPod/.test(navigator.userAgent);
+    if (isIOs) {
+        window.location = 'https://itunes.apple.com/us/app/google-tasks-get-things-done/id1353634006?mt=8';
+    } else {
+        window.location = 'https://play.google.com/store/apps/details?id=com.google.android.apps.tasks&hl=en';
+    }
+} else {
+    ReactDOM.render(<App />, document.getElementById('root'));
+}
